232d4a8bbcd5bfc1239583dbd2595482f4661f22
Juraj Zachar <juraj.zachar@bitpanda.com>
2023-10-17 22:39:38 +0200

    add mountain array implementation

--- py_ds/search/mountain_array.py
+++ py_ds/search/mountain_array.py
@@ -1,5 +1,5 @@
 """
-A mountain array is an array which satisifes these properties:
+A mountain array is an array which satisfies these properties:
   * arr.length >= 3
   * there existing some 'i' with 0 < i < arr.length -1 such that:
     * arr[0] < arr[1] < ... < arr[i-1] < arr[i] ==> (left-hand side is sorted in ascending order)
@@ -14,3 +14,48 @@ You cannot access the array directly, you may only access it using a 'MountainAr
   * MountainArray.length() ==> return the length of the array
 
 """
+
+from array import array
+
+
+class MountainArray():
+    def __init__(self, elems: [], type_code='i', ) -> None:
+        """initializes an empty MountainArray for the given type and elements"""
+        if len(elems) < 3:
+            raise RuntimeError('A MountainArray must contain at least 3 elements')
+        self.__type_code = type_code
+        self.__array = array(type_code, elems)
+        self._sort()
+
+    def get(k: int):
+        """TODO"""
+        pass
+
+    def _sort(self) -> None:
+        """sort left and right branches of this array"""
+        left = array(self.__type_code, [])
+        right = []  # list will be sorted in desc order and converted back to an array at the end
+        tmp_list = self.__array.tolist()
+        tmp_list.sort()
+        # populate left side
+        previous = None
+        for x in tmp_list:
+            if previous is None:
+                left.append(x)
+            elif previous is not None and x > previous:
+                left.append(x)
+            elif previous is not None and x == previous:
+                right.append(x)
+            previous = x
+        # right side needs sorting in rever order
+        right.sort(reverse=True)
+        # merge
+        left.extend(array(self.__type_code, right))
+        # re-assign to protected var
+        self.__array = left
+
+    def length(self):
+        return len(self.__array)
+
+    def toList(self) -> []:
+        return self.__array.tolist()
---
+++ tests/test_mountain_array.py
@@ -0,0 +1,18 @@
+import pytest
+import ctypes
+
+from py_ds.search.mountain_array import MountainArray
+
+
+@pytest.mark.parametrize("type_code, elems", [
+    ('i', [1, 2, 3, 4, 5, 3, 1]),  # signed int
+    ('I', [ctypes.c_uint(x).value for x in [1, 5, 1, 4, 2, 3, 3]]),  # unsigned integer
+    ('f', [ctypes.c_float(x).value for x in [1, 1, 2, 3, 3, 4, 5]]),  # floating-point
+    ('d', [ctypes.c_double(x).value for x in [5, 4, 3, 3, 2, 1, 1]]),  # double-precision floating point
+    ('b', [ctypes.c_int8(x).value for x in [1, 2, 3, 4, 1, 3, 5]]),  # signed integer (1 byte)
+    ('B', [ctypes.c_uint8(x).value for x in [1, 2, 5, 4, 3, 3, 1]])  # unsigned integer (1 byte)
+])
+def test_initialized_mount_array(type_code, elems):
+    candidate = MountainArray(elems, type_code)
+    assert candidate.length() == 7
+    assert candidate.toList() == [1, 2, 3, 4, 5, 3, 1]
