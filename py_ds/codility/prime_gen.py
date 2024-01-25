e8109b4247d45e735d50bcbcf42bb0ac072f1a97
Juraj Zachar <juraj.zachar@bitpanda.com>
2024-01-25 10:35:00 +0100

    add eratosthenes sieve

---
+++ py_ds/codility/prime_gen.py
@@ -0,0 +1,26 @@
+"""
+a sample solution to sieve of Eratosthenes
+"""
+from typing import List
+
+
+def solution(n: int) -> List[int]:
+    assert n > 1, "number of primes must be greater than 1"
+
+    def gen_composites(x: int) -> List[int]:
+        until = n - x
+        return [x*i for i in range(2, until)]
+
+    marked = []
+    primes = []
+    for x in range(2, n + 1):
+        if x in marked:
+            continue
+        else:
+            primes.append(x)
+            marked += gen_composites(x)
+
+    return primes
+
+if __name__ == '__main__':
+    answer = solution(100)
